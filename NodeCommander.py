da396321ad9df23855dfaf01c41c8a76bc2b50ef
dreillyEM <danielr@easymetrics.com>
2015-08-03 01:36:39 -0700

    cleaning up calling code for better modularity

--- NodeCommander.py
+++ NodeCommander.py
@@ -25,7 +25,6 @@ class NodeCommander(threading.Thread):
                   '-......-'
         '''
     def connect_node(self):
-        print "%s %s" % (self.node['key_path'],self.node['pass'])
         k = paramiko.RSAKey.from_private_key_file(self.node['key_path'],self.node['pass'])
         c = paramiko.SSHClient()
         c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -47,7 +46,6 @@ class NodeCommander(threading.Thread):
     def archive_script(self,script_path):
         new_name = script_path.replace('.cmdr', '.bkp')
         base = os.path.dirname(os.path.realpath(__file__))
-        print "mv %s %s" % (base+"/"+script_path,base+"/"+new_name)
         src = base+"/"+script_path
         dst = base+"/"+new_name
         os.rename(src,dst)
@@ -65,7 +63,6 @@ class NodeCommander(threading.Thread):
         files = {}
         ip,port = self.get_address_parts()
         try:
-            print "custom/%s/*.cmdr" % ip
             files = glob.glob("custom/%s/*.cmdr" % ip)
             fcnt = len(files)
             print "Found %d Files for %s" % (fcnt,ip)
@@ -77,22 +74,45 @@ class NodeCommander(threading.Thread):
         
         return files
     
-    def sftp_xfer(self, ssh_client, cmd_str):
-        sftp = ssh_client.open_sftp()
-        sftp.put(src, dst)
+    def run_dir(self,dirstr):
+        try:
+            flist = glob.glob(dirstr+"*.cmdr") #dirstr
+            fcnt = len(flist)
+            
+        except Exception:
+            raise
+        
+        c = self.conn
+        i = 0
+        while i < len(flist):
+            file = str(flist[i])
+            base = os.path.dirname(os.path.realpath(__file__))
+            lines = open(base+'/'+file,'r').readlines()
+            print lines
+            archive_me = False
+            #Send commands to node if they are not handled locally or comments
+            for command in lines:
+                if command is "%" :
+                    archive_me = True
+                elif command[0] is "#":
+                    continue
+                elif self.is_special(command):
+                    continue
+                else:
+                    print "Executing {}".format( command )
+                    stdin , stdout, stderr = c.exec_command(command)
+                    print stdout.read()
+                    errs = stderr.read()
+                    if errs is not None:
+                        print( "Errors:")
+                        print errs
+            if archive_me and not "common" in dirstr:
+                print "Archiving %s" % file
+                self.archive_script(file)
+            i += 1
     
-    def wait_for_it(self,seconds):
-        time.sleep(seconds) # delays for x seconds
-    
-    def run(self):
-        flist = self.get_file_list()
-      
-        if flist is None or len(flist) < 1:
-            print "No Custom Scripts for %s" % self.node['address']
-            return
-        c = self.connect_node()
-        print "connected"
-
+    def run_custom_dir(self,flist):
+        c = self.conn
         i = 0
         while i < len(flist):
             file = str(flist[i])
@@ -120,4 +140,18 @@ class NodeCommander(threading.Thread):
                 print "Archiving %s" % file
                 self.archive_script(file)
             i += 1
-        c.close()
+    def sftp_xfer(self, ssh_client, cmd_str):
+        sftp = ssh_client.open_sftp()
+        sftp.put(src, dst)
+    
+    def wait_for_it(self,seconds):
+        time.sleep(seconds) # delays for x seconds
+    
+    def run(self):
+        self.conn = self.connect_node()
+        print "connected"
+        self.run_dir("common/")
+        ip,port = self.get_address_parts()
+        cust_dir = "custom/%s/" % ip
+        self.run_dir(cust_dir)
+        self.conn.close()
